7880ce284c680066647c32256371aab1c182ef7c
Mel Maki <melody.rackham@gmail.com>
2019-10-01 17:02:01 -0600

    Finished last five events plus prevent default and stop propagation

--- js/index.js
+++ js/index.js
@@ -38,3 +38,58 @@ welcomeGreen.addEventListener(
 window.addEventListener('load', e => {
   console.log('page is fully loaded');
 });
+
+// #6 -- "resize"
+
+const vwBus = document.querySelector('.fun-bus');
+window.addEventListener('resize', () => {
+  vwBus.src = 'img/vwBus.jpg';
+});
+
+// #7 -- "scroll"
+const body = document.querySelector('body');
+window.addEventListener('scroll', () => {
+  body.style.backgroundColor = 'magenta';
+});
+
+// #8 -- "dblclick"
+
+const funBusLogo = document.querySelector('.logo-heading');
+
+funBusLogo.addEventListener('dblclick', function(e) {
+  funBusLogo.classList.toggle('large');
+});
+// #9 -- "mouse enter"
+const footer = document.querySelector('footer');
+footer.addEventListener('mouseenter', () => {
+  footer.style.transform = 'scale(3.5)';
+  footer.style.transition = 'all 0.5s';
+});
+
+// #10 -- "mouse leave "
+footer.addEventListener('mouseleave', () => {
+  footer.style.transform = 'scale(1.0)';
+  footer.style.transition = 'all 0.5s';
+});
+
+//Prevent Default
+const navLinks = document.querySelectorAll('.nav a');
+
+navLinks.forEach(i =>
+  i.addEventListener('click', e => {
+    e.preventDefault();
+  }),
+);
+
+//Stop Propagation
+
+const body2 = document.querySelector('body');
+body2.addEventListener('click', () => {
+  body2.style.backgroundColor = 'purple';
+});
+const homeColor = document.querySelector('.home');
+homeColor.addEventListener('click', e => {
+  homeColor.style.backgroundColor = 'blue';
+  console.log(body);
+  e.stopPropagation();
+});
