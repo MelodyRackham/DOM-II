33faee93b310dae226c9b7ebf2a7eadd1b6ad8f1
Mel Maki <melody.rackham@gmail.com>
2019-10-01 14:16:21 -0600

    Completed first five events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,40 @@
 // Your code goes here
+
+// #1 -- "mouse enter"
+const leftButton = document.querySelector('.btn');
+leftButton.addEventListener('mouseenter', () => {
+  leftButton.style.transform = 'scale(1.5)';
+  leftButton.style.transition = 'all 0.5s';
+});
+
+// #2 -- "mouse leave "
+leftButton.addEventListener('mouseleave', () => {
+  leftButton.style.transform = 'scale(1.0)';
+  leftButton.style.transition = 'all 0.5s';
+});
+
+//#3 -- "click"
+
+document.querySelectorAll('.nav-link').forEach(el => {
+  el.addEventListener('click', () => {
+    el.style.color = 'lightblue';
+  });
+});
+
+// #4 --"mouse over"
+const welcomeGreen = document.querySelector('h2');
+welcomeGreen.addEventListener(
+  'mouseover',
+  function(event) {
+    event.target.style.color = 'green';
+    setTimeout(function() {
+      event.target.style.color = '';
+    }, 500);
+  },
+  false,
+);
+
+// #5 -- "load"
+window.addEventListener('load', e => {
+  console.log('page is fully loaded');
+});
